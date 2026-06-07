bfc26bc6497267c358df75df88b867654eef6655
Muhammad Talha <mtalhamemon87@gmail.com>
2026-06-07 13:43:53 +0500

    Modify MongoDB connection string for security
    Updated MongoDB connection string to use placeholders for username and password.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient(
-    "mongodb+srv://talha:talha12345@cluster0.cah2qrr.mongodb.net/?appName=Cluster0"
+    "mongodb+srv://<username>:<password>@cluster0.mongodb.net//?appName=Cluster0"
 )
 
 db = client["quiz_system"]
